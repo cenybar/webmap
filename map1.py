a8c5d7975ed3c5e20297cf2f1d36a9704099b119
Santiago Fernandez <cenybar@gmail.com>
2020-06-08 12:56:42 +0200

    Minor updates

--- map1.py
+++ map1.py
@@ -1,8 +1,19 @@
 import folium
+import pandas as pd
+
+data = pd.read_csv('Volcanoes.txt')
+lat = list(data['LAT'])
+lon = list(data['LON'])
+nam = list(data['NAME'])
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-map.add_child(folium.Marker(location=[38.2,-99.1], popup='Hi I am a Marker', icon=folium.Icon(color='green')))
+
+# Para iterar en dos listas se utiliza la función ZIP
+for lt, ln, nm in zip(lat, lon, nam):
+    fg.add_child(folium.Marker(location=[lt,ln], popup=nm, icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 map.save("Map1.html")
